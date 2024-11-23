3e82f6f57725073ba0b05567c8a04db76ac2aed9
Nikola Lazarov <nikola@MacBook-Air-5.local>
2024-11-23 21:50:52 +0200

    addition(new model - service)

--- hairSalon/common/models.py
+++ hairSalon/common/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    duration = models.DurationField(help_text="Estimated time required for the service")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}|{self.duration}"
